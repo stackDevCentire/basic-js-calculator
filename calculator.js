6edc60bc4786f8719c065d704e0d6eb0451885e0
Hassan Khan <hassan@centire.in>
2020-09-29 14:14:39 +0530

    changes in javascript file

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-function doCalc(id){
+function getKeyValue(id){
     var target_id = document.getElementById(id);
     var target_id_value = target_id.textContent;
     var html_element_create = document.createElement("span");
@@ -6,3 +6,25 @@ function doCalc(id){
     html_element_create.appendChild(element_text);
     document.getElementById("calc-typed-nuber").appendChild(html_element_create);
 }
+function docalcalculation(){
+    var keyboardValue = document.getElementsByTagName("li");
+    var screenValue = document.getElementById("calc-typed-nuber").textContent;
+    var resultShow = document.getElementById("result");
+    for(i=0;i<screenValue.length;i+=1){
+        if(screenValue[i].textContent = "+"){
+            var digit = parseInt(screenValue[i]);
+            resultShow.textContent = eval(digit.textContent);
+        }
+        console.log(sum);
+    }
+    
+ 
+ 
+      
+}
+
+
+
+
+
+
